a60aaa59c9b5a139f5fbfb0f6aeb1f518cdb17a0
Anton Batalin <antbat2009@gmail.com>
2018-10-12 18:31:16 +0300

    added server

--- example/src/pages/Dashboard.tsx
+++ example/src/pages/Dashboard.tsx
@@ -34,7 +34,7 @@ interface State {
 var web3: Web3;
 
 window.addEventListener('load', function () {
-    if (typeof window.web3 !== 'undefined') {
+    if (typeof window.web3 !== 'undefined' && window.web3) {
         // Use Mist/MetaMask's provider.
         web3 = new Web3(window.web3.currentProvider);
 
