434a932f5323b6c619466d717d5a61d412144ed2
Mark Shwartzman <mark@bitclave.com>
2018-12-11 21:24:14 -0800

    heroku build

--- app.js
+++ app.js
@@ -2,5 +2,6 @@ const express = require('express');
 
 const app = express();
 app.use(express.static(__dirname + '/example/build/'));
+app.use(express.favicon(__dirname + '/example/build/favicon.ico'));
 
 app.listen(process.env.PORT || 3010);
