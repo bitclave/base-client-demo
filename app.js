56f9b569787e8c706f010d7cd9a3127a74e8dc21
Mark Shwartzman <mark@bitclave.com>
2018-12-11 21:37:19 -0800

    add favicon

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const favicon = require('serve-favicon')
 
 const app = express();
 app.use(express.static(__dirname + '/example/build/'));
-// app.use(express.favicon(__dirname + '/example/build/favicon.ico'));
+app.use(favicon(__dirname + '/example/build/favicon.ico'));
 
 app.listen(process.env.PORT || 3010);
